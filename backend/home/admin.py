1ceb41cd2e72bac5a93d834f13edef888049f1dd
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-18 10:00:11 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bnfrh
+admin.site.register(Bnfrh)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+class Bnfrh(models.Model):
+    'Generated Model'
+    xxer = models.BigIntegerField()
+    vcdb = models.BigIntegerField()
+    def test_method(self):
+        import requests
+        response = requests.get("https://hello.com")
+        data = response.json()
+        print(data)
+        return data
